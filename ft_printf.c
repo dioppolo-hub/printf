6962ce6dc2ee8db12bf9a7348dd63258c3608b4c
dioppolo <dioppolo@student.42firenze.com>
2026-01-03 23:44:25 +0100

    print_str

--- ft_printf.c
+++ ft_printf.c
@@ -11,18 +11,35 @@
 
 #include "libft.h"
 
-void print_char(va_list ap, int written)
+void print_char(va_list ap, const char *fmt, int len)
 {
 	int c;
 	c = va_arg(ap, int);
 	write(1, &c, 1);
-	written++;
+	len++;
+	fmt++;
+}
+
+void	print_str(va_list ap, const char *fmt, int len)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	while (fmt[i])
+	{
+		c = va_arg(ap, int);
+		write(1, &c, 1);
+		i++;
+	}
+	len++;
+	fmt++;
 }
 
 int ft_printf(const char *fmt, ...)
 {
 	va_list ap;
-	int written = 0;
+	int len = 0;
 
 	if (!fmt)
 		return (0);
@@ -33,40 +50,23 @@ int ft_printf(const char *fmt, ...)
 		{
 			fmt++;
 			if (*fmt == 'c')
-				print_char(ap, written);
-			else if (*fmt == '%')
-			{
-				write(1, "%", 1);
-				written++;
-			}
-			else
-			{
-				/* unsupported specifier: print it literally */
-				if (*fmt)
-				{
-					write(1, fmt, 1);
-					written++;
-				}
-			}
-			if (*fmt)
-				fmt++;
+				print_char(ap, fmt, len);
+			else if (*fmt == 's')
+				print_str(ap, fmt, len);
 		}
-		else
-		{
 			write(1, fmt, 1);
-			written++;
+			len++;
 			fmt++;
-		}
 	}
 	va_end(ap);
-	return (written);
+	return (len);
 }
 
 int main ()
 {
 	int	printlen = 0;
-	printf("mio:%d\n", ft_printf("Hello %corld%%!\n", 'W'));
-	printlen = printf("Hello %corld%%!\n", 'W');
+	printf("mio:%d\n", ft_printf("Hello %corld!\n", 'W'));
+	printlen = printf("Hello %corld!\n", 'W');
 	printf("reale:%d\n", printlen);
 	return (0);
 }
